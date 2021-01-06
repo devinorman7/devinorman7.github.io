42d87a71ab7748e2f4eb615c9410cd364085c9ef
devinorman7 <devin557@yahoo.com>
2021-01-06 10:18:21 -0500

    lots of new stuff

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-const swup = new Swup({
-    plugins: [new SwupDebugPlugin(), new SwupScriptsPlugin()]
-  });
+const options = {
+    linkSelector:
+      'a[href^="' +
+      window.location.origin +
+      '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+      plugins: [new SwupDebugPlugin(), new SwupScriptsPlugin()]
+  };
+  
+  const swup = new Swup(options);
